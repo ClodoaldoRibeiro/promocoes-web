5f0aad571dbff1e79cdec7638d82fa4ab9d281cc
Clodoaldo Ribeiro <clodoaldorib@hotmail.com>
2020-11-07 20:21:12 -0300

    Updade Promoção
    Adicionar o controlador para promoção e implementados as funções de CRUD para promoção.

--- src/main/java/com/ctisistemas/promocoesweb/web/controller/PromocaoController.java
+++ src/main/java/com/ctisistemas/promocoesweb/web/controller/PromocaoController.java
@@ -1,22 +1,34 @@
 package com.ctisistemas.promocoesweb.web.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ctisistemas.promocoesweb.domain.Categoria;
+import com.ctisistemas.promocoesweb.domain.Promocao;
 import com.ctisistemas.promocoesweb.repository.CategoriaRepository;
+import com.ctisistemas.promocoesweb.repository.PromocaoRepository;
 
 @Controller
 @RequestMapping("/promocao")
 public class PromocaoController {
 
+	private static Logger log = LoggerFactory.getLogger(PromocaoController.class);
+
 	@Autowired
 	CategoriaRepository categoriaRepository;
+
+	@Autowired
+	PromocaoRepository promocaoRepository;
 
 	@ModelAttribute("categorias")
 	public List<Categoria> getCategorias() {
@@ -26,5 +38,14 @@ public class PromocaoController {
 	@GetMapping("/add")
 	public String abrirCadastro() {
 		return "promo-add";
+	}
+
+	@PostMapping("/salvar")
+	public ResponseEntity<Promocao> salvarPromocao(Promocao promocao) {
+
+		log.info("Promocao {}", promocao.toString());
+		promocao.setDtCadastro(LocalDateTime.now());
+		promocaoRepository.save(promocao);
+		return ResponseEntity.ok().build();
 	}
 }
